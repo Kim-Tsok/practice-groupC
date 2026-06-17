7d0e03d371f2e8df7f68ddbc20b3aec720796665
ANVNYMOUS-FX <erbdool9909@gmail.com>
2026-06-17 15:20:27 +0100

    Created U25CS1003.py

--- U25CS1003.py
+++ U25CS1003.py
@@ -1,1 +1,16 @@
+import math
 
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+D = b**2 - 4*a*c
+
+if D >= 0:
+    x1 = (-b + math.sqrt(D)) / (2*a)
+    x2 = (-b - math.sqrt(D)) / (2*a)
+
+    print("Root 1 =", x1)
+    print("Root 2 =", x2)
+else:
+    print("No real roots")
